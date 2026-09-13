fa72adf9ef227811dc8627d915b8a6c864d97d4d
Nikuz Baito <nikuzbaito646@gmail.com>
2026-09-13 21:58:05 +0800

    da

--- artifacts/termipay/src/components/transaction-detail-modal.tsx
+++ artifacts/termipay/src/components/transaction-detail-modal.tsx
@@ -264,7 +264,7 @@ export function TransactionDetailModal({
                         <img
                           src={paymentMethodLogo}
                           alt={paymentMethodLabel}
-                          className="h-5 sm:h-6 w-auto max-w-[32px] object-contain shrink-0"
+                          className="h-6 sm:h-7 w-auto max-w-[38px] object-contain shrink-0"
                         />
                       )}
                       <span className="truncate">{paymentMethodLabel}</span>
